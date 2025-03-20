ccb81b192617ca4e5474339e21fb0f30a5a2d27a
My-bro <114700640+My-bro@users.noreply.github.com>
2025-03-20 10:54:27 +0100

    feat(Navbar): integrate Navbar component into RootLayout and remove from test page

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -6,9 +6,6 @@ import { AudioFileUpload } from "../../components/audio-file-upload";
 export default function Page() {
     return (
       <main className="bg-black min-h-screen">
-        <div>
-          <Navbar/>
-        </div>
         <div className="max-w-3xl mx-auto space-y-6">
           <div className="space-y-2">
             <h1 className="text-3xl font-bold tracking-tight">Audio File Upload</h1>
